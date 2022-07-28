7e846735c2f34faeb98490727c460c01d874bee8
Zen H. A-K <zen@email.com>
2022-07-28 17:47:44 +0200

    Ajout controlleur evaluateSauce

--- 2_controllers/sauceControllers.js
+++ 2_controllers/sauceControllers.js
@@ -129,29 +129,72 @@ const deleteSauce = (req, res, next) =>
 
 const evaluateSauce = (req, res, next) =>
 {
+    //Ajout dislike
     if(req.body.like === -1)
     {
-        sauceModel.updateOne({_id: req.params.id},{$inc : {dislikes: -1}, $push: {usersDisliked: req.body.userId}})
-        .then( () => 
-        {
-            //console.log(sauce.usersDisliked);
-            //console.log(sauce.usersLiked);
-            //req.body.like = 100;
-            res.status(200).json({message: "Ajout du dislike operé avec succès!"})
+        sauceModel.findOne({_id: req.params.id, usersLiked:{ $in : [req.body.userId]}}).then( currentSauce =>
+        {   
+            if (currentSauce == null)//Si il n'y a pas de présence de like sur la base de données alors ajout
+            {
+                sauceModel.updateOne({_id: req.params.id},{$inc : {dislikes: 1}, $push: {usersDisliked: req.body.userId}})
+                .then( () => res.status(200).json({message: "Ajout du dislike operé avec succès!"}))
+                .catch( error => res.status(400).json({error}) );
+            }
+
+            else
+            {
+                res.status(400).json({error});
+            }
+        
         })
-        .catch( error => res.status(400).json({error}) );
+        .catch( error => res.status(500).json({error}));
     }
 
+    //Ajout du like
     else if(req.body.like === 1)
     {
-        sauceModel.updateOne({_id: req.params.id},{$inc : {likes: 1}, $push: {usersLiked: req.body.userId}})
-        .then( () => res.status(200).json({message: "Ajout du like operé avec succès!"}))
-        .catch( error => res.status(400).json({error}) );
+        sauceModel.findOne({_id: req.params.id, usersDisliked:{ $in : [req.body.userId]}}).then( currentSauce =>
+            {   
+                //Si il n'y a pas de présence de dislike sur la base de données alors ajout like
+                if (currentSauce == null)
+                {
+                    sauceModel.updateOne({_id: req.params.id},{$inc : {likes: 1}, $push: {usersLiked: req.body.userId}})
+                    .then( () => res.status(200).json({message: "Ajout du like operé avec succès!"}))
+                    .catch( error => res.status(400).json({error}) );
+                }
+    
+                else
+                {
+                    res.status(400).json({error});
+                }
+            
+            })
+        .catch( error => res.status(500).json({error}));
     }
 
+    //Annulation de like ou dislike
     else if(req.body.like === 0)
     {
-        
+        sauceModel.findOne({_id: req.params.id, usersDisliked:{ $in : [req.body.userId]}}).then( data =>
+        {   
+            //Si le dislike est présent dans la base de données
+            if(data?.usersDisliked)
+            {
+                //Alors suppression du dit dislike
+                sauceModel.updateOne({_id: req.params.id}, {$inc : {dislikes: -1}, $pull: {usersDisliked: req.body.userId}})
+                .then( () => res.status(200).json({message: "Supression du dislike operée avec succès!"}))
+                .catch( error => res.status(400).json({error}) );
+            }
+            
+            //Sinon suppression du like de la base de données
+            else
+            {
+                sauceModel.updateOne({_id: req.params.id}, {$inc : {likes: -1}, $pull: {usersLiked: req.body.userId}})
+                .then( () => res.status(200).json({message: "Supression du like operée avec succès!"}))
+                .catch( error => res.status(400).json({error}) );
+            }
+        })
+        .catch( error => res.status(500).json({error}));
     }
 
 }
